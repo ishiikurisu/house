4c65ec22d0f9682aaa1b78a0a40ba8c59f43cb69
Cristiano Silva Jr <cristiano.a.junior@ieee.org>
2017-02-03 19:53:38 -0200

    Building script for distribution

--- bin/house.min.lua
+++ bin/house.min.lua
@@ -734,96 +734,121 @@ if always_try_using_lpeg then
 end
 
 -- BASIC CONTROLLER DEFINITION
-local basic_controller = { }
-
 basic_controller.construct = function(args)
-    local self = { }
+  local self = { }
 
-    self.args = args
-    self.repo = args[1]
-    self.script_name = os.tmpname()
-    self.shell = "sh"
-    if self.script_name:sub(1, 1) == "\\" then
-        self.shell = "cmd /C"
-        self.script_name = "." .. self.script_name .. "bat"
-    end
+  self.args = args
+  self.repo = args[1]
+  self.script_name = os.tmpname()
+  self.shell = "sh "
+  if self.script_name:sub(1, 1) == "\\" then
+    self.shell = "cmd /C "
+    self.script_name = "." .. self.script_name .. "bat"
+  end
 
-    return self
+  return self
 end
 
 basic_controller.new = function(args)
-    local self = basic_controller.construct(args)
+  local self = basic_controller.construct(args)
 
-    self.draw = function()
-        print(self.args)
+  self.draw = function()
+    print(self.args)
+  end
+
+  self.buildTree = function(repo, midaction)
+    local dirs = util.mysplit(repo, "/")
+    local levels = #dirs
+    local commands = { }
+
+    table.insert(commands, "cd src")
+    for _, d in ipairs(dirs) do
+      table.insert(commands, "cd " .. d)
+    end
+    commands = midaction(commands)
+    for i = 1, levels do
+      table.insert(commands, "cd ..")
     end
 
-    self.execute = function(commands)
-        local fp = io.open(self.script_name, "w")
-        io.output(fp)
-        for _, cmd in ipairs(commands) do
-            io.write(cmd .. "\n")
-        end
-        io.close(fp)
-        os.execute(self.shell .. " " .. self.script_name)
-        os.remove(self.script_name)
-    end
+    return commands
+  end
 
-    return self
+  self.execute = function(commands)
+    local fp = io.open(self.script_name, "w")
+    io.output(fp)
+    for _, cmd in ipairs(commands) do
+      io.write(cmd .. "\n")
+    end
+    io.close(fp)
+    os.execute(self.shell .. self.script_name)
+    os.remove(self.script_name)
+  end
+
+  return self
 end
 
 -- LOAD CONTROLLER DEFINITION
 local load_controller = { }
 
+load_controller.construct = function(args)
+  local self = basic_controller.new(args)
+
+  self.command = "git pull origin master"
+
+  return self
+end
+
 load_controller.new = function(args)
-    local self = basic_controller.new(args)
+  local self = load_controller.construct(args)
 
-    self.draw = function()
-        local dirs = util.mysplit(self.args[1], "/")
-        local levels = #dirs
-        local commands = { }
+  self.draw = function()
+    local commands = { }
+    local repo = self.args[1]
 
-        table.insert(commands, "cd src")
-        for _, d in ipairs(dirs) do
-            table.insert(commands, "cd " .. d)
-        end
-        table.insert(commands, "git pull origin master")
-        for i = 1, levels do
-            table.insert(commands, "cd ..")
-        end
-
-        self.execute(commands)
+    if repo ~= nil then
+      commands = self.buildTree(repo, self.addCommands)
+    else
+      table.insert(commands, self.command)
     end
 
-    return self
+    self.execute(commands)
+  end
+
+  self.addCommands = function(commands)
+    table.insert(commands, self.command)
+    return commands
+  end
+
+  return self
 end
 
 -- UPLOAD CONTROLLER DEFINITION
 local upload_controller = { }
 
 upload_controller.new = function(args)
-    local self = basic_controller.new(args)
+  local self = basic_controller.new(args)
 
-    self.draw = function()
-        local dirs = util.mysplit(self.args[1], "/")
-        local levels = #dirs
-        local commands = { }
+  self.draw = function()
+    local commands = { }
+    local repo = self.args[1]
 
-        table.insert(commands, "cd src")
-        for _, d in ipairs(dirs) do
-            table.insert(commands, "cd " .. d)
-        end
-        table.insert(commands, "git add -A")
-        table.insert(commands, "git commit")
-        table.insert(commands, "git push origin master")
-        for i = 1, levels do
-            table.insert(commands, "cd ..")
-        end
-
-        self.execute(commands)
+    if repo ~= nil then
+      commands = self.buildTree(repo, self.addCommands)
+    else
+      commands = self.addCommands(commands)
     end
 
-    return self
+    self.execute(commands)
+  end
+
+  self.addCommands = function(commands)
+    table.insert(commands, "git add -A")
+    table.insert(commands, "git commit")
+    table.insert(commands, "git push origin master")
+    return commands
+  end
+
+  return self
 end
 
 -- GET CONTROLLER DEFINITION
@@ -891,6 +916,35 @@ build_controller.new = function(args)
   return self
 end
 
+-- EDIT CONTROLLER DEFINITION
+local edit_controller = { }
+
+edit_controller.construct = function(args)
+  local self = basic_controller.new(args)
+
+  -- Loading configuration file
+  local configpath = './src/' .. self.repo .. '/.houseconfig'
+  local config = util.readAll(configpath)
+  self.params = JSON.decode(config, 1, nil).edit
+
+  return self
+end
+
+edit_controller.new = function(args)
+  local self = edit_controller.construct(args)
+
+  self.draw = function()
+    local where = ' src/' .. self.repo
+    local command = self.params.editor .. where .. ' &'
+    local commands = { }
+    table.insert(commands, command)
+    self.execute(commands)
+  end
+
+  return self
+end
+
+return edit_controller
 
 -- HOUSE DEFINITION
 local house = { }
--- house/controllers/basic_controller.lua
+++ house/controllers/basic_controller.lua
@@ -1,56 +1,56 @@
 local basic_controller = { }
 
 basic_controller.construct = function(args)
-    local self = { }
+  local self = { }
 
-    self.args = args
-    self.repo = args[1]
-    self.script_name = os.tmpname()
-    self.shell = "sh"
-    if self.script_name:sub(1, 1) == "\\" then
-        self.shell = "cmd /C"
-        self.script_name = "." .. self.script_name .. "bat"
-    end
+  self.args = args
+  self.repo = args[1]
+  self.script_name = os.tmpname()
+  self.shell = "sh "
+  if self.script_name:sub(1, 1) == "\\" then
+    self.shell = "cmd /C "
+    self.script_name = "." .. self.script_name .. "bat"
+  end
 
-    return self
+  return self
 end
 
 basic_controller.new = function(args)
-    local self = basic_controller.construct(args)
+  local self = basic_controller.construct(args)
 
-    self.draw = function()
-        print(self.args)
+  self.draw = function()
+    print(self.args)
+  end
+
+  self.buildTree = function(repo, midaction)
+    local dirs = util.mysplit(repo, "/")
+    local levels = #dirs
+    local commands = { }
+
+    table.insert(commands, "cd src")
+    for _, d in ipairs(dirs) do
+      table.insert(commands, "cd " .. d)
+    end
+    commands = midaction(commands)
+    for i = 1, levels do
+      table.insert(commands, "cd ..")
     end
 
-    self.buildTree = function(repo, midaction)
-      local dirs = util.mysplit(repo, "/")
-      local levels = #dirs
-      local commands = { }
+    return commands
+  end
 
-      table.insert(commands, "cd src")
-      for _, d in ipairs(dirs) do
-        table.insert(commands, "cd " .. d)
-      end
-      commands = midaction(commands)
-      for i = 1, levels do
-        table.insert(commands, "cd ..")
-      end
-
-      return commands
+  self.execute = function(commands)
+    local fp = io.open(self.script_name, "w")
+    io.output(fp)
+    for _, cmd in ipairs(commands) do
+      io.write(cmd .. "\n")
     end
+    io.close(fp)
+    os.execute(self.shell .. self.script_name)
+    os.remove(self.script_name)
+  end
 
-    self.execute = function(commands)
-        local fp = io.open(self.script_name, "w")
-        io.output(fp)
-        for _, cmd in ipairs(commands) do
-            io.write(cmd .. "\n")
-        end
-        io.close(fp)
-        os.execute(self.shell .. " " .. self.script_name)
-        os.remove(self.script_name)
-    end
-
-    return self
+  return self
 end
 
 return basic_controller
