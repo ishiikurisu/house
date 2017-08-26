36ddde497d8dd23340ee3ca71738fc50e327bbfe
Cris Silva Jr <ishiikurisu@yahoo.com>
2017-08-26 19:26:49 -0300

    Updating main program

--- main/main.go
+++ main/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+    "os"
     "fmt"
     "github.com/ishiikurisu/house"
 )
 
 func main() {
+    controller := house.Generate(os.Args)
+    output, oops := controller.Execute()
+
     fmt.Println("---")
-    // TODO Write this monster
-    fmt.Printf("%s\n", house.SayHi())
+    if oops == nil {
+        fmt.Println(output)
+    } else {
+        fmt.Printf("Oops: %s\n", oops)
+    }
     fmt.Println("...")
 }
